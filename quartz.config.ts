fb6121d71e1accb93e0030676f5a6532f096c833
André Videla <andre.videla@gmail.com>
2026-04-07 12:36:47 +0100

    add github action for pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Container Compendium",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "andrevidela.github.io/container-compendium-site",
     ignorePatterns: ["private", "templates", ".obsidian",
           "vault/build", "vault/tex", "vault/docgen", "vault/.obsidian", "vault/font", "vault/Images", "vault/Illustrations"],
     defaultDateType: "modified",
